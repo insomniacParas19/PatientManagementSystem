1b949d3e0ca630dc8d40ae3c096b21c7aa01fc12
inso558 <parasbam3@gmail.com>
2023-07-26 07:07:17 +0545

    Department Model Add

---
+++ PMS/Models/Department.cs
@@ -0,0 +1,9 @@
+public class Department
+{
+    public int Id {get; set; }
+    public string Name {get; set; }
+    public string Description {get; set; }
+    public string Established {get; set; }
+
+
+}
